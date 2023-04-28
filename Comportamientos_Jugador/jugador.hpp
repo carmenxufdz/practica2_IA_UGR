3f14e3b1601cdf78b96fecd868115c09ddeb3460
Carmenchu <carmenxufdz@correo.ugr.es>
2023-04-29 01:26:36 +0200

    Tutorial V2

--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -5,14 +5,47 @@
 
 #include <list>
 
+struct stateN0{
+  ubicacion jugador;
+  ubicacion sonambulo;
+
+  bool operator== (const stateN0 &x) const{
+    return (jugador == x.jugador 
+    and sonambulo.f == x.sonambulo.f 
+    and sonambulo.c == sonambulo.c);
+
+  }
+};
+
+struct nodeN0{
+  stateN0 st;
+  list<Action> secuencia;
+
+  bool operator== (const nodeN0 &n) const{
+    return (st == n.st);
+  }
+
+  bool operator< (const nodeN0 &n) const {
+    if (st.jugador.f < n.st.jugador.f)
+      return true;
+    else if (st.jugador.f == n.st.jugador.f and st.jugador.c < n.st.jugador.c)
+      return true;
+    else if (st.jugador.f == n.st.jugador.f and st.jugador.c == n.st.jugador.c and st.jugador.brujula < n.st.jugador.brujula)
+      return true;
+    else 
+      return false;
+  }
+};
 
 class ComportamientoJugador : public Comportamiento {
   public:
     ComportamientoJugador(unsigned int size) : Comportamiento(size) {
       // Inicializar Variables de Estado
+      hayPlan = false;
     }
     ComportamientoJugador(std::vector< std::vector< unsigned char> > mapaR) : Comportamiento(mapaR) {
       // Inicializar Variables de Estado
+      hayPlan = false;
     }
     ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
     ~ComportamientoJugador(){}
@@ -20,9 +53,16 @@ class ComportamientoJugador : public Comportamiento {
     Action think(Sensores sensores);
     int interact(Action accion, int valor);
 
+    void VisualizaPlan(const stateN0 &st, const list<Action> &plan);
+
 
   private:
     // Declarar Variables de Estado
+    list<Action> plan;    // Almacena el plan en ejecucion
+    bool hayPlan;         // TRUE: esta siguiendo un plan
+    stateN0 c_state;      // Estado nvl 0
+    ubicacion goal;       // Posicion a la que se debe llegar
+    vector<vector<unsigned char>> matrizConPlan;
 
 
 
